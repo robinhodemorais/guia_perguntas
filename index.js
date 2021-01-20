6eddaee8583af6a2b1cc5bb415b64adeaae3d574
Robinho de Morais <robinhodemorais@gmail.com>
2021-01-19 22:37:03 -0300

    Estruturas condicionais com EJS

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ app.set('view engine','ejs');
 app.get("/:nome/:lang",(req,res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
-
+    var exibitMsg = false;
     res.render("index", {
         nome:nome,
         lang:lang,
         empresa:"Guia do Programador",
-        inscritos:8000
+        inscritos:8000,
+        msg:exibitMsg
     });
 });
 
