f5c6a5c79ff7cebf81e0cff892f5d615aea82276
Robinho de Morais <robinhodemorais@gmail.com>
2021-01-19 22:30:13 -0300

    Exibindo variaveis no HTML

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 //estou dizendo para express usar o EJS como View Engine
 app.set('view engine','ejs');
 
-app.get("/",(req,res) => {
-    res.render("index");
+app.get("/:nome/:lang",(req,res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+
+    res.render("index", {
+        nome:nome,
+        lang:lang,
+        empresa:"Guia do Programador",
+        inscritos:8000
+    });
 });
 
 app.listen(8080,() => {
